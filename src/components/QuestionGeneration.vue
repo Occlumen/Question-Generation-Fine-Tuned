7d23248c489787abca85d64a94cf5b5e695cfdf6
Occlumen <82927164+Occlumen@users.noreply.github.com>
2024-12-04 15:09:08 +0530

    Update QuestionGeneration.vue

--- src/components/QuestionGeneration.vue
+++ src/components/QuestionGeneration.vue
@@ -45,6 +45,7 @@ const submitForm = () => {
     let file = selectedFile.value;
     let formData = new FormData();
     formData.append('file', file);
+    console.log("fetch");
     const apiUrl = process.env.VUE_APP_QUESTION_GENERATION;
     fetch(apiUrl, {
       method: 'POST',
